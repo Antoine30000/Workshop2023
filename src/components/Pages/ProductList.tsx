9b6f0d532205dcc78d9f7e066fd9c268b8355dc3
Antoine 3000 <antoine@beaurin.net>
2023-05-30 16:28:44 +0200

    wip: product grid

--- src/components/Pages/ProductList.tsx
+++ src/components/Pages/ProductList.tsx
@@ -2,9 +2,9 @@ import { mockData } from "../mockdata/data"
 
 export const ProductList = () => {
     return (
-        <div>
+        <div className="product-grid">
         {mockData.map((product) => (
-          <div key={product.id}>
+          <div key={product.id} className="product-item">
             {/* <img src={product.image} alt={product.name} /> */}
             <h3>{product.name}</h3>
             <p>{product.description}</p>
