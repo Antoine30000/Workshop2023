9ec187a138f62f3db21129dcad8d3750a0206350
Antoine 3000 <antoine@beaurin.net>
2023-05-30 15:01:05 +0200

    wip: homepage

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return (
+      <header>
+        <div className="logo">
+          <img src="placeholder_logo.png" alt="Logo" />
+        </div>
+        <nav>
+         
+        </nav>
+      </header>
+    );
+  };
